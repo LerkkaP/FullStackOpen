76fec9fe8fc9f45bc725453bb41656f28e065e5c
Erik Peteri <peteri.erik@gmail.com>
2023-05-17 10:38:01 +0300

    blogilista step 2

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blog')
 
 const mongoUrl = config.MONGODB_URI
 
@@ -11,5 +12,7 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,23 +1,5 @@
 const app = require('./app')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
