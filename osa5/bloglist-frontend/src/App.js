b9f2bd57d42ab8d874fe7e892ba4df5ca656b9de
Erik Peteri <peteri.erik@gmail.com>
2023-06-19 20:43:27 +0300

    exercise 5.3

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -5,6 +5,11 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setnewTitle] = useState('')
+  const [newAuthor, setnewAuthor] = useState('')
+  const [newUrl, setnewUrl] = useState('')
+
+
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -28,6 +33,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -36,7 +42,7 @@ const App = () => {
   }
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault()  
 
   try {
     const user = await loginService.login({
@@ -47,6 +53,7 @@ const App = () => {
       'loggedBlogappUser', JSON.stringify(user)
     ) 
 
+    blogService.setToken(user.token)
     setUser(user)
     setUsername('')
     setPassword('')
@@ -55,34 +62,103 @@ const App = () => {
   }
 }
 
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+          console.log(returnedBlog)
+        setBlogs(blogs.concat(returnedBlog))
+        setnewTitle('')
+      })
+  }
+
+  const handleTitleChange = (event) => {
+    setnewTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setnewAuthor(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setnewUrl(event.target.value)
+  }
+
+
+  const loginForm = () => (
+    <div>
+    <h2>Log in to application</h2>
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input 
+        type='text'
+        value={username}
+        name="Username"
+        onChange={({target}) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+        type='password'
+        value={password}
+        name='Password'
+        onChange={({target}) => setPassword(target.value)}
+        />
+      </div>
+      <div>
+        <button type='submit'>login</button>
+      </div>
+    </form>
+  </div>
+  )
+
+  const blogForm = () => (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          title:
+          <input 
+          type='text'
+          value={newTitle}
+          onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          author:
+          <input 
+          type='text'
+          value={newAuthor}
+          onChange={handleAuthorChange} 
+          />
+        </div>
+        <div>
+          url:
+          <input 
+          type='text'
+          value={newUrl}
+          onChange={handleUrlChange}
+          />
+        </div>
+        <div>
+          <button type="submit">create</button>
+        </div>
+      </form>
+    </div>
+  )
+
   if (user === null) {
     return (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input 
-            type='text'
-            value={username}
-            name="Username"
-            onChange={({target}) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-            type='password'
-            value={password}
-            name='Password'
-            onChange={({target}) => setPassword(target.value)}
-            />
-          </div>
-          <div>
-            <button type='submit'>login</button>
-          </div>
-        </form>
-      </div>
+      loginForm()
     )
   }
 
@@ -92,6 +168,7 @@ const App = () => {
       <p>{user.name} logged in 
         <button onClick={handleLogout}>logout</button>
       </p>
+      {blogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog}/>
       )}
