364de7444b005bc1894be8e58cb0a5bd7d651c48
Erik Peteri <peteri.erik@gmail.com>
2023-07-03 15:58:48 +0300

    exercise 5.9

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,11 @@
 import './Togglable';
 import Togglable from './Togglable';
+import { useState } from 'react'; // Import the useState hook
+import blogService from '../services/blogs';
 
 const Blog = ({ blog, user }) => {
+  const [updatedBlog, setUpdatedBlog] = useState(blog)
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -10,18 +14,28 @@ const Blog = ({ blog, user }) => {
     marginBottom: 5
   };
 
+  const handleLike = async () => {
+    const updatedLikes = updatedBlog.likes + 1
+    const updatedBlogData = { ...updatedBlog, likes: updatedLikes }
+
+    await blogService.update(updatedBlog.id, updatedBlogData)
+
+    setUpdatedBlog(updatedBlogData)
+  }
+
   return (
     <div style={blogStyle}>
-      {blog.title} {blog.author}
+      {updatedBlog.title} {updatedBlog.author} 
       <Togglable buttonLabel="view">
-        <div>{blog.url}</div>
+        <div>{updatedBlog.url}</div>
         <div>
-          {"likes 0"} <button>like</button>
+          {`likes ${updatedBlog.likes}`} <button onClick={handleLike}>like</button>
         </div>
         <div>{user.name}</div>
       </Togglable>
     </div>
-  );
-};
+  )
+}
 
-export default Blog;
+export default Blog
+
--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -22,5 +22,15 @@ const create = async newObject => {
 
 }
 
+const update = async (id, updatedObject) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.put(`${baseUrl}/${id}`, updatedObject, config)
+  return response.data
+}
+
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, create, setToken  }
+export default { getAll, create, setToken, update  }
