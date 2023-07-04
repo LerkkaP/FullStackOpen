ff0ac3bbacd1ec541f6d28838ca005f559e19a09
Erik Peteri <peteri.erik@gmail.com>
2023-07-04 20:39:59 +0300

    exercise 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 
@@ -23,4 +24,32 @@ test('renders blog title and author, but not URL and likes by default', () => {
     const authorElement = screen.getByText(/test author/i);
     expect(titleElement).toBeInTheDocument();
     expect(authorElement).toBeInTheDocument();
+  });
+
+test('renders url, likes and user when view button is pressed', () => {
+    const blog = {
+        id: 1,
+        title: 'Test Blog',
+        author: 'Test Author',
+        url: 'https://example.com',
+        likes: 10,
+    };
+    
+    const user = {
+        name: 'Test User',
+    };
+
+
+    render(<Blog blog={blog} user={user} />);
+
+    const button = screen.getByText('view');
+    userEvent.click(button);
+  
+    const urlElement = screen.getByText('https://example.com');
+    const likesElement = screen.getByText('likes 10');
+    const userElement = screen.getByText('Test User');
+  
+    expect(urlElement).toBeInTheDocument();
+    expect(likesElement).toBeInTheDocument();
+    expect(userElement).toBeInTheDocument();
   });
