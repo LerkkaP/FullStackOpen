7c182919bfcebc8c0ba1cc7f822cebb72e86a26a
Erik Peteri <peteri.erik@gmail.com>
2023-07-12 13:49:16 +0300

    6.10

--- osa6/redux-anecdotes/src/components/Filter.js
+++ osa6/redux-anecdotes/src/components/Filter.js
@@ -1,23 +1,25 @@
-import { filterChange } from "../reducers/filterReducer"
-import { useDispatch } from 'react-redux'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { filterChange } from '../reducers/filterReducer';
 
 const Filter = () => {
   const dispatch = useDispatch()
 
-    const handleChange = (event) => {
-      const input = event.target.value
-      dispatch(filterChange(input))
-    }
-    const style = {
-      marginBottom: 10
-    }
-  
-    return (
-      <div style={style}>
-        filter <input onChange={handleChange} />
-      </div>
-    )
+  const handleChange = (event) => {
+    const input = event.target.value
+    dispatch(filterChange(input))
   }
-  
-  export default Filter
+
+  const style = {
+    marginBottom: 10,
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Filter
 
--- osa6/redux-anecdotes/src/reducers/filterReducer.js
+++ osa6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,15 @@
-const filterReducer = (state = '', action) => {
+import { createSlice } from '@reduxjs/toolkit'
 
-    switch (action.type) {
-      case 'SET_FILTER':
-        return action.payload
-      default:
-        return state
-    }
-  }
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterChange: (state, action) => {
+      return action.payload;
+    },
+  },
+})
 
-export const filterChange = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
+export const { filterChange } = filterSlice.actions
 
-export default filterReducer
+export default filterSlice.reducer
