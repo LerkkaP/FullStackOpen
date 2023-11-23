f6f88bd76a049199eab0309082dcc48cee42ea6b
Erik Peteri <peteri.erik@gmail.com>
2023-11-23 20:15:09 +0200

    exercise 9.3

--- osa9/9.1-9.3/exerciseCalculator.ts
+++ osa9/9.1-9.3/exerciseCalculator.ts
@@ -39,4 +39,25 @@ const calculateExercises = (hours: number[], target: number): resultObject => {
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+  if (process.argv.length < 4) {
+    throw new Error("Not enough arguments");
+  }
+  const array_hour = process.argv.slice(3);
+  const hours: number[] = array_hour.map((hour) => Number(hour));
+  const target: number = Number(process.argv[2]);
+
+  const allNumbers = array_hour.every((hour) => !isNaN(Number(hour)));
+
+  if (!isNaN(Number(target)) && allNumbers) {
+    console.log(calculateExercises(hours, target));
+  } else {
+    throw new Error("Provided values were not numbers");
+  }
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
