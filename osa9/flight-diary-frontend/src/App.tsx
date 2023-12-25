6173279cccff6e42df7424fbb2cf13c6f73c6d62
Erik Peteri <peteri.erik@gmail.com>
2023-12-25 12:04:41 +0200

    exercise 9.18

--- osa9/flight-diary-frontend/src/App.tsx
+++ osa9/flight-diary-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Diary } from "./types";
 import diaryService from "./services/diaries";
+import axios from "axios";
 
 const App = () => {
   const [diaries, setDiaries] = useState<Diary[]>([]);
@@ -8,6 +9,8 @@ const App = () => {
   const [visibility, setVisibility] = useState('');
   const [weather, setWeather] = useState('');
   const [comment, setComment] = useState('');
+  const [message, setMessage] = useState('');
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     const fetchDiariesList = async () => {
@@ -17,23 +20,50 @@ const App = () => {
     void fetchDiariesList();
   }, [])
 
+  useEffect(() => {
+    if(!message){
+     setShow(false)
+     return
+    }
+    setShow(true)
+    const timer = setTimeout(() => {
+      setShow(false)
+    }, 2500);
+    return () => clearTimeout(timer);
+  }, [message])
+
   const diaryCreation = async (event: React.SyntheticEvent) => {
     event.preventDefault()
-    const newDiary = await diaryService.createDiary({
-      date: date,
-      visibility: visibility,
-      weather: weather,
-      comment: comment,
-    });
-    setDiaries([...diaries, newDiary]);
-    setDate("")
-    setVisibility("")
-    setWeather("")
-    setComment("")
+    try {
+      const newDiary = await diaryService.createDiary({
+        date: date,
+        visibility: visibility,
+        weather: weather,
+        comment: comment
+      })
+
+      setDiaries([...diaries, newDiary]);
+      setDate("")
+      setVisibility("")
+      setWeather("")
+      setComment("")
+    } catch(error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response !== undefined) {
+          const errorMessage = error.response.data.split(".")[1]
+          setMessage(errorMessage)
+        }
+      } else {
+        console.error(error);
+      }
+    }
   }
   return (
   <div>
     <h2>Add new entry</h2>
+    {show && (
+      <p style={{color: "red"}}>{message}</p>
+    )}
     <form onSubmit={diaryCreation}>
       <div>
         date<input value={date} onChange={(event) => setDate(event.target.value)}/>
