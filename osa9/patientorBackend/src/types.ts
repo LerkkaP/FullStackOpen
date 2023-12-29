a0bf2510719524b5ff148b0adde05dbc825a2e8d
Erik Peteri <peteri.erik@gmail.com>
2023-12-29 10:22:06 +0200

    exercise 9.22

--- osa9/patientorBackend/src/types.ts
+++ osa9/patientorBackend/src/types.ts
@@ -1,7 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
-
 export enum Gender {
   male = "male",
   female = "female",
@@ -14,6 +10,16 @@ export interface Diagnosis {
   latin?: string;
 }
 
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
 export interface Patient {
   id: string;
   name: string;
@@ -23,6 +29,42 @@ export interface Patient {
   occupation: string;
   entries: Entry[];
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave; 
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
